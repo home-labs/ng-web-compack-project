0e977586952861ab120938e225915558a3c11d9c
Rafael Laurindo <rafaelplaurindo@gmail.com>
2019-01-30 01:58:53 -0200

    type

--- projects/ng-web-compack/src/lib/side-panel/component.ts
+++ projects/ng-web-compack/src/lib/side-panel/component.ts
@@ -64,7 +64,7 @@ export class SidePanelComponent implements OnInit, OnDestroy {
 
     private toggleHorizontalSliding() {
         let
-            containerParent: Node,
+            containerParent: HTMLElement,
             containerClone,
             computedStyle: CSSStyleDeclaration,
             inlineStyle: CSSStyleDeclaration,
